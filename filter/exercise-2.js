8ed1dee9201fcaef547f8caa66089cef38e44907
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-11-11 15:25:54 +0100

    Update exercise-2.js

--- filter/exercise-2.js
+++ filter/exercise-2.js
@@ -43,32 +43,3 @@ const filteredUsers = users.filter(
 );
 
 console.log(filteredUsers);
-
-const card = [
-  {
-    id: 01,
-    work: "cleaner",
-    name: "john",
-  },
-  {
-    id: 02,
-    work: "designer",
-    name: "olav",
-  },
-  {
-    id: 03,
-    work: "guard",
-    name: "rita",
-  },
-];
-
-const cardArray = card.map((value) => `I am ${value.name}`);
-console.log(cardArray);
-
-// const newArray = cardArray.filter((person) => {
-//   if (person.work === "designer") {
-//     return true;
-//   }
-// });
-
-// console.log(newArray);
